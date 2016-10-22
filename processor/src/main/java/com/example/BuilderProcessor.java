ef1cdf3032d2f4e96625219a31dda1eb6e1ec8d7
Flo <wengelef@gmail.com>
2016-10-22 20:31:22 +0200

    chore (processor): adds Processor metadata

--- processor/src/main/java/com/example/BuilderProcessor.java
+++ processor/src/main/java/com/example/BuilderProcessor.java
@@ -4,8 +4,10 @@ import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.lang.model.element.TypeElement;
 
+@SupportedAnnotationTypes("com.example.Builder")
 public class BuilderProcessor extends AbstractProcessor {
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
