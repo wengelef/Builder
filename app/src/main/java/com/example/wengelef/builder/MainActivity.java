8dee8ad53a1d608a070d870abfc4656e1635bd84
Flo <wengelef@gmail.com>
2016-10-23 15:29:01 +0200

    chore : test Builder with Sample model

--- app/src/main/java/com/example/wengelef/builder/MainActivity.java
+++ app/src/main/java/com/example/wengelef/builder/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.wengelef.builder;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        user = new UserBuilder()
+                .age(12)
+                .mail("wengelef@gmail.com")
+                .username("wengelef")
+                .build();
+
+        TextView textView = (TextView) findViewById(R.id.textview);
+        textView.setText(String.format("Username: %s \nEmail: %s \nAge: %d", user.username, user.mail, user.age));
     }
 }
---
+++ app/src/main/java/com/example/wengelef/builder/User.java
@@ -0,0 +1,14 @@
+package com.example.wengelef.builder;
+
+import com.example.Builder;
+
+/**
+ * Created by Flo on 22.10.2016.
+ */
+@Builder
+public class User {
+
+    String username;
+    String mail;
+    int age;
+}
